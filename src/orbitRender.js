8182dd8295985e3ae5c1873c310aa9c3291e43e3
조재용 <jaeyong@mobidoo.co.kr>
2022-04-17 20:15:50 +0900

    자전과 공전 씬 구현

---
+++ src/orbitRender.js
@@ -0,0 +1,110 @@
+import * as THREE from 'three'
+import Stats from 'stats.js'
+import Dat from 'dat.gui'
+
+export const renderView = () => {
+	/* 요소 가져오기 */
+	const canvasEl = document.querySelector('canvas')
+
+	/* 카메라 설정 */
+	const camera = new THREE.PerspectiveCamera(
+		75, 																		// fov
+		window.innerWidth / window.innerHeight, // aspect 
+		0.1, 																		// near
+		1000, 																	// far
+	)
+	camera.position.set(3, 4, 5)
+	camera.zoom = 0.5
+  camera.lookAt(0, 0, 0)
+	camera.updateProjectionMatrix()
+
+	/* 빛 설정 */
+	const ambientLight = new THREE.AmbientLight('#fff', 0.5) // color, intensity
+	const directionalLight = new THREE.DirectionalLight('#fff', 1) // color, intensity
+	directionalLight.position.set(1, 1, 2)
+
+	/* 축과 그리드 시각화 설정 */
+	const axesHelper = new THREE.AxesHelper(10) // length
+	const gridHeler = new THREE.GridHelper(10) // length
+
+	/* 메쉬 설정 */
+  const geometry = new THREE.SphereGeometry()
+  function getSphereMesh(color, size) {
+    const material = new THREE.MeshStandardMaterial({color})
+    const mesh =  new THREE.Mesh(geometry, material)
+    mesh.scale.set(size, size, size)
+    return mesh
+  }
+  const sunMesh = getSphereMesh('orange', 2)
+  const earthMesh = getSphereMesh('blue', 0.3)
+  const moonMesh = getSphereMesh('gray', 0.15)
+  const rotationMeshGroup = new THREE.Group()
+  rotationMeshGroup.position.set(0, 0, 0)
+  const innerRotationMeshGroup = new THREE.Group()
+  innerRotationMeshGroup.position.set(4.5, 0, 0)
+  moonMesh.position.set(0.5, 0, 0)
+  innerRotationMeshGroup.add(earthMesh, moonMesh)
+  rotationMeshGroup.add(sunMesh, innerRotationMeshGroup)
+
+	/* 씬 설정 */
+	const scene = new THREE.Scene()
+	scene.background = new THREE.Color(BACGROUND_COLOR)
+	scene.add(camera)
+	scene.add(ambientLight)
+	scene.add(directionalLight)
+	scene.add(axesHelper)
+	scene.add(gridHeler)
+  scene.add(rotationMeshGroup)
+
+	/* 렌더러 설정 */
+	const renderer = new THREE.WebGLRenderer({ 
+		canvas: canvasEl, 
+		antialias: true,
+		alpha: true,
+	})
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1)
+	renderer.render(scene, camera)
+
+	/* 이벤트 설정 */
+	function handleResize() {
+		camera.aspect = window.innerWidth / window.innerHeight
+		camera.updateProjectionMatrix()
+		renderer.setSize(window.innerWidth, window.innerHeight)
+		renderer.render(scene, camera)
+	}
+	window.addEventListener('resize', handleResize)
+
+	/* FPS 시각화 설정 */
+	const stats = new Stats()
+	document.body.append(stats.domElement)
+
+	/* 애니메이션 설정 */
+	const clock = new THREE.Clock()
+	function rotate() {
+    const delta = clock.getDelta()
+    rotationMeshGroup.rotation.y += delta
+    innerRotationMeshGroup.rotation.y += delta
+		moonMesh.rotation.y += delta
+  }
+	function repeatAnimation() {
+		rotate()
+    camera.lookAt(0, 0, 0)
+		renderer.render(scene, camera)
+		window.requestAnimationFrame(repeatAnimation) // 실행할 애니메이션 함수 안에 requestAnimationFrame함수를 호출함으로써 반복 동작 구현
+		stats.update() // FPS 업데이트
+	}
+	function initAnimation() {
+		repeatAnimation()
+	}	
+	initAnimation()
+
+	/* 컨트롤러 설정 */
+	const controller = new Dat.GUI()
+	controller.add(camera.position, 'x', -5, 5, 0.01).name('X Position of a the camera')
+  controller.add(camera.position, 'y', -5, 5, 0.01).name('Y Position of a the camera')
+  controller.add(camera.position, 'z', -5, 5, 0.01).name('Z Position of a the camera')
+}
+
+/* 상수 */
+const BACGROUND_COLOR = "#fff"
